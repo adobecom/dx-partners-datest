7d6562bfb029966f2a2c1aead2448b64effb0fe2
dejanpreradovic <cod47516@adobe.com>
2024-06-26 09:20:37 +0200

    remove join now basic example

--- libs/mep/loggedInExperience.js
+++ libs/mep/loggedInExperience.js
@@ -1,1 +1,12 @@
-console.log('Hello from inserted script');
+const removeJoinNowCta = () => {
+  console.log('Remove join now cta');
+  const cookies = document.cookie.split(';').map(cookie => cookie.trim());
+  const partnerDataCookie = cookies.find(cookie => cookie.startsWith('partner_data='));
+  if (!partnerDataCookie) return;
+  const cookieValue = JSON.parse(decodeURIComponent(partnerDataCookie.substring(('partner_data=').length).toLowerCase()));
+  if (!cookieValue || !cookieValue.status || cookieValue.status === 'member') return;
+  const joinNowCta = document.querySelector('.marquee .con-button');
+  joinNowCta.remove();
+}
+
+removeJoinNowCta();
