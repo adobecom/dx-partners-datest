10e7b495423a704800f0108d143f51ee9adbfe89
Slobodanka <hit12337@adobe.com>
2025-04-16 11:36:21 +0200

    Timeout test fix

--- nala/blocks/news/news.test.js
+++ nala/blocks/news/news.test.js
@@ -371,7 +371,7 @@ test.describe('Validate news block', () => {
       await newTab.goto(`${path}`);
       const newTabPage = new NewsPage(newTab);
 
-      await page.locator('a[href*="/solution-partners/news_archive.html/"]:has-text("Explore past articles")').waitFor({ state: 'visible' });
+      await page.locator('a[href*="/solution-partners/news_archive.html"]:has-text("Explore past articles")').waitFor({ state: 'visible' });
 
       await newTabPage.firstCardDate.waitFor({ state: 'visible', timeout: 30000 });
       const resultCards = await newTabPage.resultNumber.textContent();
